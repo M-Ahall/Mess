f357dae267555ba3bee1f4a713cc5d98bf9ed863
Magnus Åhall <magnus@ahall.se>
2022-01-05 15:08:23 +0100

    Start of websocket

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Mess
-Manage messages, primarily debug messages.
 
-Messages are sent to an HTTP URL, formatted with json.
+
--- frontend/js/socket.js
+++ frontend/js/socket.js
@@ -3,6 +3,73 @@ var _socket = null;
 var _socketRequestNum = 0;
 var _connectionCallbackClosed = null;
 
+
+class WebSocket {
+	socket;
+	endpoint = '';
+	hooks = {};
+
+	static domainWsAddress() {
+		let proto = '';
+		switch(location.protocol) {
+			case 'http:':  proto = 'ws';  break;
+			case 'https:': proto = 'wss'; break;
+			default:
+				alert('Unknown protocol, aborting.');
+		}
+		return `${proto}://${location.host}/ws`;
+	}
+
+	constructor() {
+		this.endpoint = WebSocket.domainWsAddress();
+		this.hooks = {
+			open:    [],
+			close:   [],
+			error:   [],
+			message: [],
+		}
+
+		// Create the websocket this application will use.
+		this.socket = new WebSocket(this.endpoint);
+
+		// Connect events to websocket
+		this.socket.onopen    = (evt)=>this.hooks.open.forEach((hook, _)=>hook(evt));
+		this.socket.onclose   = (evt)=>this.hooks.close.forEach((hook, _)=>hook(evt));
+		this.socket.onerror   = (evt)=>this.hooks.error.forEach((hook, _)=>hook(evt));
+		this.socket.onmessage = (evt)=>this.hooks.message.forEach((hook, _)=>hook(evt));
+
+		/*o
+			console.log("socket connected");
+			if(typeof socketConnected == 'function')
+				socketConnected();
+		}
+
+		this.socket.onclose = function(e) {
+			console.log("socket close", e);
+			if(_connectionCallbackClosed !== null)
+				_connectionCallbackClosed();
+		}
+
+		this.socket.onerror = function(e) {
+			console.log("socket error", e);
+		}
+
+		this.socket.onmessage = function(e) {
+			let msg = JSON.parse(e.data)
+			msgHandler(msg);
+		}
+		*/
+	}
+
+	addHook(type, func) {
+		if(this.hooks[type] === undefined)
+			throw "Unknown hook type";
+
+		this.hooks[type].push(func);
+	}
+
+}
+
 function socketOnClose(func) {
 	_connectionCallbackClosed = func;
 }
